cd121328062190b7959fb961a217c0e0e340a5d7
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-08-20 11:57:05 -0300

    minor fixes

--- schemas.py
+++ schemas.py
@@ -1,17 +1,21 @@
 from pydantic import BaseModel
 from datetime import date
 
+
 class TransactionBase(BaseModel):
-  concept: str
-  category: str
-  quantity: int
-  resolved: bool
-  
+    concept: str
+    category: str
+    quantity: int
+    resolved: bool
+
+
 class TransactionCreate(TransactionBase):
-  pass
+    pass
+
 
 class Transaction(TransactionBase):
-  id: int
-  created: date
-  class Config:
-    orm_mode = True
+    id: int
+    created: date
+
+    class Config:
+        orm_mode = True
