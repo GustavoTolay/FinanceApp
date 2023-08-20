eafb14487baf750a304dcd273aa070f664eae7fe
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-08-20 05:50:04 -0300

    add db schemas

---
+++ schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from datetime import date
+
+class TransactionBase(BaseModel):
+  concept: str
+  category: str
+  quantity: int
+  resolved: bool
+  
+class TransactionCreate(TransactionBase):
+  pass
+
+class Transaction(TransactionBase):
+  id: int
+  created: date
+  class Config:
+    orm_mode = True
