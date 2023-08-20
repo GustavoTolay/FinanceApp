5273b877b5c007a89501e17ea9dd0e14d6727d4e
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-08-20 05:49:42 -0300

    add db models

---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Boolean, Column, Integer, String, Date
+from datetime import date
+
+from .database import Base
+
+class Transaction(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    concept = Column(String)
+    category = Column(String)
+    quantity = Column(Integer)
+    resolved = Column(Boolean, default=False)
+    created = Column(Date, default=date.today())
