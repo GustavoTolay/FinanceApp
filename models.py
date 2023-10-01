65a0eb3da45fe350ff38da0782460ecb7c4ca2d4
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-10-01 19:45:45 -0300

    remake transactions model

--- models.py
+++ models.py
@@ -1,16 +1,20 @@
 from sqlalchemy import Boolean, Column, Integer, String, Date
 from datetime import date
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from typing import Optional, List
 
 class Base(DeclarativeBase):
     pass
 
 class TransactionModel(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    concept = Column(String)
-    category = Column(String)
-    quantity = Column(Integer)
-    resolved = Column(Boolean, default=False)
-    created = Column(Date, default=date.today())
+    __tablename__ = "transactions"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    concept: Mapped[str] = mapped_column(String)
+    # category: Mapped["Category"] = relationship()
+    quantity: Mapped[int] = mapped_column(Integer)
+    resolved: Mapped[bool] = mapped_column(Boolean, default=False)
+    created: Mapped[date] = mapped_column(Date, default=date.today())
+    
+    # def __repr__(self) -> str:
+        # return f"User(id={self.id!r}, concept={self.concept!r}, category={self.category!r})"
