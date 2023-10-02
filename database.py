bc67d1bafef16de4cc4c29b07554b785a7d223e5
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-10-02 12:51:05 -0300

    add db connection

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from models import Base
 
 DATABASE_URL = "sqlite:///./record.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(DATABASE_URL, echo=True)
+
+Base.metadata.create_all(engine)
