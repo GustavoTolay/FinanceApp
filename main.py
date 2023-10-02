d1459b22566ab6ee2f6ea296f7888224a2b016e0
Gustavo Tolay <jeremiastolay1714@gmail.com>
2023-10-02 15:35:14 -0300

    category schema

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
-from schemas import TransactionCreate, Transaction
 from routers import transactions
-import services.transactions as tr
 
 app = FastAPI()
 app.include_router(transactions.router)
